cc09ce7431c03ccb27b423dbce7b9c4ecaf77f9d
Berkay Kara <47315483+BerkayKara@users.noreply.github.com>
2020-02-25 19:59:07 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# seniorProject
-Back-end of our senior project
+First you have to connect to mysql database with wamp
+"npm i" is the command to download all necessary files
+"nodemon server.js" is the command to run backend.
---
+++ server.js
@@ -0,0 +1,363 @@
+//Creating the server 
+//Berkay Kara
+
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(session({ secret: 'secret' }))
+
+
+app.listen(8080, (err) => {
+    console.log("Server is running at port 8080");
+});
+
+var mysqlConnection = mysql.createConnection({
+    password: '',
+    user: 'root',
+    database: 'bilsportdb',
+    host: 'localhost',
+    multipleStatements: true,
+});
+
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('DB connection Successfull');
+    else
+        console.log('DB connection failed');
+});
+
+
+
+
+//Announcement********************************************************************
+
+//Get all announcements
+app.get('/announcements', (req, res) => {
+    mysqlConnection.query('SELECT * FROM announcements', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+
+//Get an announcement
+app.get('/announcements/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM announcements WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+//Delete an announcement
+app.delete('/announcements/:id', (req, res) => {
+    console.log(req.params);
+    mysqlConnection.query('DELETE FROM announcements WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Deleted successfully');
+        else
+            console.log(err);
+    });
+});
+
+//Insert an announcement
+app.post('/announcement', (req, res) => {
+    let announcement = req.body;
+    console.log(announcement);
+    var sql = "SET @text = ?; SET @date = ?;CALL insertAnnouncementProcedure(@text,@date);";
+    mysqlConnection.query(sql, [announcement.text, announcement.date], (err, rows, fields) => {
+        if (!err)
+            res.send('Announcement Inserted');
+        else
+            console.log("err");
+    });
+});
+
+
+
+//Update an announcement
+app.put('/announcements', (req, res) => {
+    let announcement = req.body;
+    var sql = "SET @id = ?; SET @text = ?; SET @date = ?;CALL updateAnnouncementProcedure(@id, @text,@date);";
+    mysqlConnection.query(sql, [announcement.id, announcement.text, announcement.date], (err, rows, fields) => {
+        if (!err)
+            res.send('Updated successfully');
+        else
+            console.log(err);
+    });
+});
+
+//Statistics***************************************************************
+
+//Get all statistics
+app.get('/statistics', (req, res) => {
+    mysqlConnection.query('SELECT * FROM statistics', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+
+//Get a statistics
+app.get('/statistics/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM statistics WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+//Delete a statistics
+app.delete('/statistics/:id', (req, res) => {
+    console.log(req.params);
+    mysqlConnection.query('DELETE FROM statistics WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Deleted successfully');
+        else
+            console.log(err);
+    });
+});
+
+//Insert an statistics
+app.post('/statistics', (req, res) => {
+    let statistics = req.body;
+    var sql = "SET @id = ?; SET @km = ? ;SET @sett = ?;CALL insertStatisticProcedure(@id,@km,@sett);";
+    mysqlConnection.query(sql, [statistics.id, statistics.km, statistics.sett], (err, rows, fields) => {
+        if (!err)
+            res.send('Inserted statistics id: ' + statistics.id);
+        else
+            console.log(err);
+    });
+});
+
+
+//Update an statistics
+app.put('/statistics', (req, res) => {
+    let statistics = req.body;
+    var sql = "SET @id = ?; SET @km = ?; SET @sett = ?;CALL updateStatisticProcedure(@id, @km,@sett);";
+    mysqlConnection.query(sql, [statistics.id, statistics.km, statistics.sett], (err, rows, fields) => {
+        if (!err)
+            res.send('Updated successfully');
+        else
+            console.log(err);
+    });
+});
+
+
+
+
+// Login
+app.post('/login', function (req, res) {
+    console.log(req.body);
+    var id = req.body.user.id;
+    var password = req.body.user.password;
+    mysqlConnection.query('SELECT * FROM account WHERE id = ? AND password = ?', [id, password], (err, rows, fields) => {
+        if (!err) {
+            if (rows.length > 0)
+                res.send(rows);
+            else
+                res.send(404);
+        }
+        else {
+            res.send(err);
+        }
+    });
+}
+);
+
+
+
+
+
+
+//Pool***************************************************************
+
+//Get all pool lanes
+app.get('/pool', (req, res) => {
+    mysqlConnection.query('SELECT * FROM pool', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+//Get a pool lane
+app.get('/pool/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM pool WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+//Delete a pool lane
+app.delete('/pool/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM pool WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Deleted successfully');
+        else
+            console.log(err);
+    });
+});
+
+//Insert a pool lane
+app.post('/pool', (req, res) => {
+    let pool = req.body;
+    var sql = "SET @id = ?; SET @lane = ? ;SET @quota = ?;CALL insertPoolProcedure(@id,@lane,@quota);";
+    mysqlConnection.query(sql, [pool.id, pool.lane, pool.quota], (err, rows, fields) => {
+        if (!err)
+            res.send('Inserted pool id: ' + pool.id);
+        else
+            console.log(err);
+    });
+});
+
+
+//Update an pool
+app.put('/pool', (req, res) => {
+    let pool = req.body;
+    var sql = "SET @id = ?; SET @lane = ?; SET @quota = ?;CALL updatePoolProcedure(@id, @lane,@quota);";
+    mysqlConnection.query(sql, [pool.id, pool.lane, pool.quota], (err, rows, fields) => {
+        if (!err)
+            res.send('Updated successfully');
+        else
+            console.log(err);
+    });
+});
+
+
+
+
+//Squash***************************************************************
+
+//Get all squash courts
+app.get('/squash', (req, res) => {
+    mysqlConnection.query('SELECT * FROM squash', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+//Get a squash court
+app.get('/squash/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM squash WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+
+//Delete a squash court
+app.delete('/squash/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM squash WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Deleted successfully');
+        else
+            console.log(err);
+    });
+});
+
+//Insert a squash court
+app.post('/squash', (req, res) => {
+    let squash = req.body;
+    var sql = "SET @id = ?; SET @courtNo = ? ;SET @available = ?;CALL insertSquashProcedure(@id,@courtNo,@available);";
+    mysqlConnection.query(sql, [squash.id, squash.courtNo, squash.available], (err, rows, fields) => {
+        if (!err)
+            res.send('Inserted squash id: ' + squash.id);
+        else
+            console.log(err);
+    });
+});
+
+
+//Update an squash
+app.put('/squash', (req, res) => {
+    let squash = req.body;
+    var sql = "SET @id = ?; SET @courtNo = ?; SET @available = ?;CALL updateSquashProcedure(@id, @courtNo,@available);";
+    mysqlConnection.query(sql, [squash.id, squash.courtNo, squash.available], (err, rows, fields) => {
+        if (!err)
+            res.send('Updated successfully');
+        else
+            console.log(err);
+    });
+});
+
+
+
+
+
+
+//Tennis***************************************************************
+
+//Get all tennis courts
+app.get('/tennis', (req, res) => {
+    mysqlConnection.query('SELECT * FROM tennis', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+//Get a tennis court
+app.get('/tennis/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM tennis WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+
+//Delete a tennis court
+app.delete('/tennis/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM tennis WHERE id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Deleted successfully');
+        else
+            console.log(err);
+    });
+});
+
+
+//Insert a tennis court
+app.post('/tennis', (req, res) => {
+    let tennis = req.body;
+    var sql = "SET @id = ?; SET @court = ? ;SET @campus = ?;CALL insertTennisProcedure(@id, @court,@campus);";
+    mysqlConnection.query(sql, [tennis.id, tennis.court, tennis.campus], (err, rows, fields) => {
+        if (!err)
+            res.send('Inserted tennis id: ' + tennis.id);
+        else
+            console.log(err);
+    });
+});
+
+
+//Update an tennis
+app.put('/tennis', (req, res) => {
+    let tennis = req.body;
+    var sql = "SET @id = ?; SET @court = ?; SET @campus = ?;CALL updateTennisProcedure(@id, @court,@campus);";
+    mysqlConnection.query(sql, [tennis.id, tennis.court, tennis.campus], (err, rows, fields) => {
+        if (!err)
+            res.send('Updated successfully');
+        else
+            console.log(err);
+    });
+});
