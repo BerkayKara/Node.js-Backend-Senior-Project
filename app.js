e8f0150ef8f47d0ce9430122122089c27c69b305
Berkay Kara <berkaykara1997@gmail.com>
2020-05-05 21:39:16 +0300

    CORS Policy issue solved

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use((req,res,next) => {
-    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Origin","http://localhost:8081");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
-    if(req.method ==="OPTIONS"){
-        res.header("Access-Control-Allow-Methods","PUT","POST","PATCH","DELETE","GET");
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods","PUT, POST, PATCH, DELETE, GET");
         return res.status(200).json({}); 
-    }
+    } next();
 
 });
 
@@ -139,7 +139,6 @@ app.post('/upload', upload, (req,res,err) => {
 
 });
 
-// app.use(bodyParser.json());
 
 
 
